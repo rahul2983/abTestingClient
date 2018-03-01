738b24a54f5af3edfc07ea1a8015e5792ae7b0e5
rahul2983 <rahul.uoa@gmail.com>
2018-02-28 23:06:08 -0800

    Using HostListener to load iframe successfully

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { InputInfo } from '../models/input-info';
 import { LoadUrlService } from '../services/load-url.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
@@ -9,27 +9,19 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./input-form.component.css']
 })
 export class InputFormComponent implements OnInit {
-  @ViewChild("pageDOM", {read: ElementRef}) pageDOM: ElementRef;
-  @ViewChild("testURLVal", {read: ElementRef}) testURLVal: ElementRef;
   inputInfo = new InputInfo('http://www.espncricinfo.com/');
   submitted = false;
   loaded = false;
   safeURL: SafeResourceUrl;
 
-  constructor(private loadUrlService: LoadUrlService, private sanitizer: DomSanitizer) { }
+  constructor(private loadUrlService: LoadUrlService, private sanitizer: DomSanitizer, private el: ElementRef) { }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit() {
     this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.inputInfo.url);
   }
 
-  getDomInfo() {
-    if (this.pageDOM.nativeElement) {
-      console.log(this.pageDOM.nativeElement);
-    // console.log(this.pageDOM.nativeElement);
-    }
+  ngAfterViewInit() {
+    // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.inputInfo.url);
   }
 
   onSubmit() {
@@ -41,9 +33,10 @@ export class InputFormComponent implements OnInit {
     return JSON.stringify(this.inputInfo);
   }
 
-  onLoad() {
-    this.loaded = true;
-    this.getDomInfo();
+  @HostListener('onload') onLoad() {
+    let iframeElem = this.el.nativeElement.querySelector('iframe');
+    console.log(iframeElem);
+    console.log(iframeElem.querySelector('html'));
+    
   }
-  
 }
