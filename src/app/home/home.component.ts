2beb31036bb5eb3035c8c355fdba4fa742b8ed2a
rahul2983 <rahul.uoa@gmail.com>
2018-03-20 22:40:28 -0700

    fixing the delete button to delete the correct table row

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadUrlService } from '../services/load-url.service';
 import { InputInfo } from '../models/input-info';
 
@@ -10,11 +10,8 @@ import { SimpleChanges } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('tableRow') tableRow: ElementRef;
-  numAbTests: number;
   abTests: Array<InputInfo>;
   clicked: boolean = false;
-  abtest_to_delete: InputInfo;
 
   constructor(private loadUrlService: LoadUrlService) { }
 
@@ -29,17 +26,12 @@ export class HomeComponent implements OnInit {
   getAllAbTests() {
     this.loadUrlService.getAllAbTests().subscribe(res => {
       this.abTests = res['data'];
-      this.numAbTests = res['data'].length;
       console.log(res['data']);
     }); 
   }
 
-  deleteAbTest() {
-    // Being able to delete only the first row for now
-    // Need to code for refreshing the homepage after deleting an entry - Maybe add a modal or force a hard refresh
-    console.log(this.tableRow.nativeElement.rowIndex);
-    // console.log(this.abTests[index]);
-    this.loadUrlService.deleteAbTest(this.abTests[0]).subscribe(res => {
+  deleteAbTest(rowIndex: number) {
+    this.loadUrlService.deleteAbTest(this.abTests[rowIndex]).subscribe(res => {
       this.getAllAbTests();  
     });
   }
